17f438de339e2bb4fa136d90a3fadcf23436d3e3
Osandoval42 <maxwellsandstein@gmail.com>
2017-06-19 00:59:19 -0700

    dsa

---
+++ unmastered/algos/lsd_radix_sort.js
@@ -0,0 +1,67 @@
+const charsToIdxs = {0: 0, 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5};
+
+function lsdRadixSort(arr){
+	let aux = new Array(arr.length);
+	let d = longestStrLen(arr) - 1;
+	let charCounts = new Array(Object.keys(charsToIdxs).length + 1);
+
+	while (d >= 0){
+		console.log("ran");
+		charCounts.forEach((count, idx) => {
+			charCounts[idx] = 0;
+		})
+
+		arr.forEach((str) => {
+			const char = (d < str.length) ? str[d] : 0;
+			(charCounts[charsToIdxs[char] + 1])++;
+		})
+		console.log(`d is ${d}`);
+		showCharCounts(charCounts);
+
+		for (let i = 1; i < charCounts.length; i++){
+			charCounts[i] += charCounts[i - 1];
+		}
+
+		arr.forEach((str) => {
+			const char = (d < str.length) ? str[d] : 0;
+			const charIdx = charsToIdxs[char];
+
+			aux[charCounts[charIdx]] = str;
+			(charCounts[charIdx])++;
+		})
+
+		aux.forEach((str, i) => {
+			arr[i] = str;
+		})
+
+		d--;
+	}
+}
+
+function showCharCounts(charCounts){
+	charCounts.forEach((count, i) => {
+		console.log(`${i} : ${count}`);
+	})
+}
+
+function longestStrLen(arr){
+	let longestLen = 0;
+	arr.forEach((el) => {
+		if (el.length > longestLen){
+			longestLen = el.length;
+		}
+	})
+	return longestLen;
+}
+
+function test(){
+	let strs = ["abcde", "cabde", "acbde", "acbed", "cadbe", "cadeb", "cabde", "ccbde"];
+	strs = strs.map((str) => {
+		return str.split("");
+	})
+	lsdRadixSort(strs);
+	console.log(strs);
+}
+
+test();
+
